435bed1992904a3f177e4db3d5da600e74de79d5
Rafael Bueno <rafaelbuenog2020@gmail.com>
2022-10-03 20:40:07 -0300

    Update Calc2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  operacao = '';
+  resultado = '';
+
   constructor() {}
+
+  adicionarValor(valor: any){
+    this.operacao += valor;
+  }
+
+  limparMemoria(){
+    this.operacao = '';
+    this.resultado = '';
+  }
+
+  limparOperacao(){
+    this.operacao = '';
+  }
+
+  inverterValor(){
+    // Alterna o número entre positivo e negativo
+  }
+
+  apagarCaracter(){
+    if (this.operacao.length > 0) {
+    this.operacao = this.operacao.substring(0, this.operacao.length - 1);
+    }
+  }
 
 }
