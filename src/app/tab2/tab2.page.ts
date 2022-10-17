757e5cd6940eb8a9c74d34b45f18e59e4f84bf85
Rafael Bueno <rafaelbuenog2020@gmail.com>
2022-10-17 20:33:41 -0300

    Calc2 finalizada
    Biblioteca adicionada para executar a operação, e correção de erros no input

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { evaluate} from 'mathjs'
 
 @Component({
   selector: 'app-tab2',
@@ -9,20 +10,43 @@ export class Tab2Page {
 
   operacao = '';
   resultado = '';
+  numero = false;
+  caracter = true;
+  caracteres = ['.', '/', '*', '+', '-']; 
 
   constructor() {}
 
+  ngOnInit() {}
+
+  realizarOperacao(){
+    try {
+      this.resultado = evaluate(this.operacao);
+    } catch (err) {
+      this.resultado = 'Inválido!'; 
+    }
+  }
+
   adicionarValor(valor: any){
-    this.operacao += valor;
+    this.caracter = this.caracteres.includes(valor);
+
+    if (!this.caracter) {
+      this.operacao += valor;
+      this.numero = true;
+    } else if (this.caracter && this.numero) {
+      this.operacao += valor;
+      this.numero = false;
+    }
   }
 
   limparMemoria(){
     this.operacao = '';
     this.resultado = '';
+    this.numero = false;
   }
 
   limparOperacao(){
     this.operacao = '';
+    this.numero = false;
   }
 
   inverterValor(){
@@ -32,6 +56,17 @@ export class Tab2Page {
   apagarCaracter(){
     if (this.operacao.length > 0) {
     this.operacao = this.operacao.substring(0, this.operacao.length - 1);
+    }
+
+    const ultimo = this.operacao.substring(this.operacao.length, 1);
+    this.caracter = this.caracteres.includes(ultimo);
+    
+    console.log(ultimo);
+
+    if (!this.caracter) {
+      this.numero = true;
+    } else {
+      this.numero = false;
     }
   }
 
